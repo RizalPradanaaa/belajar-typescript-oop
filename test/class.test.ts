85b753e445b5b9e821414bae0f11b842bd47d6dd
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-04-09 14:23:38 +0700

    Class

---
+++ test/class.test.ts
@@ -0,0 +1,9 @@
+describe("Class", () => {
+  class Customer {}
+  class Order {}
+
+  it("should support class", () => {
+    const customer: Customer = new Customer();
+    const order = new Order();
+  });
+});
