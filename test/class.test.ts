7a8f6e715d71a38c989616c33dcef37b77697e94
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-04-09 14:30:43 +0700

    Constructor

--- test/class.test.ts
+++ test/class.test.ts
@@ -1,9 +1,18 @@
 describe("Class", () => {
-  class Customer {}
+  class Customer {
+    constructor() {
+      console.info("Create new user");
+    }
+  }
   class Order {}
 
   it("should support class", () => {
     const customer: Customer = new Customer();
     const order = new Order();
+  });
+
+  it("should support constructor", () => {
+    new Customer();
+    new Customer();
   });
 });
