dd58c2d9d01c78713231d597067bab3b1d455fe5
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-04-09 14:40:01 +0700

    Method

--- test/properties.test.ts
+++ test/properties.test.ts
@@ -7,6 +7,10 @@ describe("Properties", () => {
     constructor(id: number, name: string) {
       (this.id = id), (this.name = name);
     }
+
+    sayHello(name: string): void {
+      console.info(`Hello ${name}, my name is ${this.name}`);
+    }
   }
 
   it("should can have properties", () => {
@@ -15,5 +19,10 @@ describe("Properties", () => {
     console.info(customer.id);
     console.info(customer.name);
     console.info(customer.age);
+  });
+
+  it("should can have method", () => {
+    const customer: Customer = new Customer(1, "Rizal");
+    customer.sayHello("Boy");
   });
 });
